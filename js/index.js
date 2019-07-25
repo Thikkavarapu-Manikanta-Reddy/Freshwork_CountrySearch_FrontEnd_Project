3a8d9e603d1a90c8fdc3e8f2040e0183cedf1472
THIKKAVARAPU MANIKANTA REDDY <t.manikantareddy160@gmail.com>
2019-07-24 23:38:50 -0700

    SecondCommit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', () => {
+    document.getElementById("search-bar")
+        .addEventListener("keyup", (event) =>{
+            event.preventDefault();
+            countrysearch();
+            if(event.keyCode === 13)
+            countrysearch();   
+    });
+});
+function countrysearch(){
+
+    let search = document.getElementById("search-bar").value;
+    const baseurl = new Request("https://restcountries.eu/rest/v2/name/" + search);
+
+    fetch(baseurl).then((response) => {
+        if(response.status === 200)
+            return response.json();
+       else
+       {
+        const searchdata = document.getElementById("search-results")
+        while(searchdata.firstChild){
+            searchdata.removeChild(searchdata.firstChild)
+        }
+        alert("No Country Found!!");
+        throw new Error("Data not found!!");
+       }
+    }).then(response => {
+        //Todo dispaly search results
+        console.log(response);
+        const searchdata = document.getElementById("search-results")
+    while(searchdata.firstChild){
+        searchdata.removeChild(searchdata.firstChild)
+    }
+    for(i=0; i<response.length; i++){
+        let card = document.createElement("div");
+        card.className = "card";
+
+        let flagimage = document.createElement("img");
+        flagimage.setAttribute("src", response[i]["flag"]);
+        flagimage.className = "flagimage";
+        card.appendChild(flagimage);
+        
+        let countryname = document.createElement("h4");
+        countryname.innerHTML = response[i]["name"];
+        countryname.className = "countryname";
+        card.appendChild(countryname);
+        
+        let cardlink = document.createElement("a");
+        cardlink.className = "a";
+        cardlink.setAttribute("href", "countrydetails.html?countryname="+response[i]["name"]);
+        cardlink.appendChild(card);
+
+        searchdata.appendChild(cardlink);
+    }
+    
+    }).catch(error => {
+        //Todo Report Error to user
+        console.error(error);
+    })
+}
