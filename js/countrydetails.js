0eb11ce60cd90e83f71ba764ea8bf12e4bc0139e
THIKKAVARAPU MANIKANTA REDDY <t.manikantareddy160@gmail.com>
2019-07-25 01:15:14 -0700

    FourthCommit

--- js/countrydetails.js
+++ js/countrydetails.js
@@ -12,5 +12,14 @@ function getParameterByName(name, url) {
 }
 function countrydetails(){
 console.log(JSON.parse(getParameterByName('countryname')));
-
+var countrydetails = JSON.parse(getParameterByName('countryname'));
+const countrydata = document.getElementById("country-details")
+var table = document.createElement('table');
+table.innerHTML = "  <tr><th>Name</th><th>Capital</th><th>Region</th><th>SubRegion</th><th>Population</th></tr><tr><td>"
++countrydetails.name+"</td><td>"
++countrydetails.capital+"</td><td>"
++countrydetails.region+"</td><td>"
++countrydetails.subregion+"</td><td>"
++countrydetails.population+"</td></tr>";
+countrydata.appendChild(table);
 }
